108f5630ddf86f6eea5682dfaf8c145d2b533bf1
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-11 22:41:30 +0300

    Add play and pause functionality to SongCard component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,8 +5,18 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, isPlaying, activeSong, data, i }) => (
-  <div>SongCard</div>
-);
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch();
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(playPause(true));
+    dispatch(setActiveSong(song, data, i));
+  };
+
+  <div>SongCard</div>;
+};
 
 export default SongCard;
