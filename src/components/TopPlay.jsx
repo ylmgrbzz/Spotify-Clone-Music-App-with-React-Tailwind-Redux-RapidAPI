553872e7ecbe052f1f9571b5f1bd8d3f83fc6a98
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-13 21:42:01 +0300

    Refactor TopPlay component styling

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -19,25 +19,23 @@ const TopChartCard = ({
   handlePlayClick,
 }) => (
   <div
-    className={` flex-row items-center   hover:bg-[#4c426e] ${
-      activeSong?.title === song?.title
-        ? "bg-[#4c426e]"
-        : "bg-transparent py-2 p-4 rounded-lg cursor-pointer mb-2"
-    } `}
+    className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${
+      activeSong?.title === song?.title ? "bg-[#4c426e]" : "bg-transparent"
+    } py-2 p-4 rounded-lg cursor-pointer mb-2`}
   >
-    <h3 className="font-bold text-base text-white mr-3  ">{i + 1}.</h3>
-    <div className="flex-1 flex flew-row justify-between items-center">
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
       <img
-        className=" w-20 h-20 rounded-lg  "
+        className="w-20 h-20 rounded-lg"
         src={song?.images?.coverart}
         alt={song?.title}
       />
-      <div className="text-xl font-bold text-white">
+      <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}>
-          <p className="">{song?.title}</p>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
         </Link>
         {/* <Link to={`/artists/${song?.artists[0].adamid}`}>
-          <p className="">{song?.subtitle}</p>
+          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
         </Link> */}
       </div>
     </div>
